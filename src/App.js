8cb771acd0e04a92cfe54ee626f337b8ad5ae643
Shih-coding <100893145+Shih-coding@users.noreply.github.com>
2022-09-08 11:38:54 +0800

    added Github repo URL and portfolio website

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
-import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by {""}
+        <a href="https://tourmaline-croissant-4da783.netlify.app/" target="_blank" rel="noreferrer">
+        Rebecca Shih</a> and is {""}
+        <a href="https://github.com/Shih-coding/weather-react"
+         target="_blank" rel="noreferrer">
+          
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
 
-export default App;
