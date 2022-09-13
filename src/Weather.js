41aa823a95ed1cc05c95c6dc643216577c19c592
Shih-coding <100893145+Shih-coding@users.noreply.github.com>
2022-09-14 00:33:02 +0800

    Added react weather icons

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
+import ReactAnimatedWeather from "react-animated-weather";
 
 import "./App.css"
 import "./Weather.css";
@@ -19,7 +20,7 @@ export default function Weather(props) {
       wind: response.data.wind.speed,
       city: response.data.name,
       description: response.data.weather[0].description,
-      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      iconUrl: response.data.weather[0].icon,
     });
    
   }
--- src/Weatherinfo.js
+++ src/Weatherinfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon"
 
 export default function WeatherInfo(props) {
   return (
@@ -15,13 +16,12 @@ export default function WeatherInfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           <div className="clearfix">
-            <img
-              src={props.data.iconUrl}
-              alt={props.data.description}
-              className="float-left"
-            />
+            <div className="float-left">
+              <WeatherIcon code={props.data.icon}/>
+            
             <span className="temperature">{Math.round(props.data.temperature)}</span>
-            <span className="unit">°C</span>
+              <span className="unit">°C</span>
+            </div>
           </div>
         </div>
         <div className="col-6">
