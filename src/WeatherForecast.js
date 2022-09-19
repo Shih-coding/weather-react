77c33f5a556bff94769cc0bc4074d3d3afec7d5e
Shih-coding <100893145+Shih-coding@users.noreply.github.com>
2022-09-19 21:57:06 +0800

    Finished editing forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,12 +1,18 @@
-import React, {useState} from "react";
-import WeatherIcon from "./WeatherIcon";
+import React, {useState, useEffect }from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
+import { cleanup } from "@testing-library/react";
+
 
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+  
 
   function handleResponse(response) {
     setForecast(response.data.daily);
@@ -16,12 +22,18 @@ export default function WeatherForecast(props) {
   
   if (loaded) {
      return (
-   <div className="WeatherForecast">
-      <div className="row">
-        <div className="col">
-             <WeatherForecastDay data={forecast[0]} />
-        </div>
-      </div>
+       <div className="WeatherForecast">
+         <div className="row">
+           {forecast.map(function (dailyForecast, index) {
+             if (index < 5) {
+               return (
+                 <div className="col" key={index}>
+                   <WeatherForecastDay data={dailyForecast} />
+                 </div>
+            
+             ); }
+           })}
+         </div>
        </div>
      );
   } else {
