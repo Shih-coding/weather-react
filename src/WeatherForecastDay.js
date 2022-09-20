2ad7359d5b783e089d67bc5c2b448c36fa4fc526
Shih-coding <100893145+Shih-coding@users.noreply.github.com>
2022-09-21 00:14:44 +0800

    retro pixel CSS styling

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -22,13 +22,16 @@ export default function WeatherForecastDay(props) {
   
   return (
     <div>
-  <div className="WeatherForecastDay">
-               {day()}
-          </div>
-          <WeatherIcon code={props.data.weather[0].icon} size={36} />
-          <div className="WeatherForecast-temperatures">
-               <span className="WeatherForecast-temperature-max">{maxTemperature()}</span>
-               <span className="WeatherForecast-temperature-min">{minTemperature()}</span>
+      <div className="WeatherForecastDay">{day()}</div>
+      <WeatherIcon code={props.data.weather[0].icon} size={36} />
+      <div className="WeatherForecast-temperatures">
+        <span className="WeatherForecast-temperature-max">
+          {maxTemperature()}|
+        </span>
+      
+        <span className="WeatherForecast-temperature-min">
+          {minTemperature()}
+        </span>
       </div>
     </div>
   );
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -2,12 +2,13 @@ import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
 import WeatherTemperature from "./WeatherTemperature";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
 
 
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
-      
       <h1>{props.data.city}</h1>
       <ul>
         <li>
@@ -16,19 +17,18 @@ export default function WeatherInfo(props) {
         <li className="text-capitalize">{props.data.description}</li>
       </ul>
       <div className="row mt-3">
-
-        <div className="col-8">
+        <div className="col-7">
           <div className="float-left">
-            <WeatherIcon code={props.data.icon} size={56} />
+            <WeatherIcon code={props.data.icon} size={60} />
           </div>
           <div>
-              <WeatherTemperature celsius={props.data.temperature} />
-         </div>
+            <WeatherTemperature celsius={props.data.temperature} />
+          </div>
         </div>
-        <div className="col-4">
+        <div className="col-5 weather-extra" >
           <ul>
-            <li> Humidity: {props.data.humidity}%</li>
-            <li> Wind: {props.data.wind}km/h</li>
+            <li> <FontAwesomeIcon icon={solid("droplet") }size="xl" /> Humidity: {props.data.humidity}%</li>
+            <li><FontAwesomeIcon icon={solid("wind")} size="lg"/> Wind: {props.data.wind}km/h</li>
           </ul>
         </div>
       </div>
